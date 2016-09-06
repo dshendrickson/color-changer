6f1ca708a610b345b20e6ec77629b077d3b8e6db
Dale S Hendrickson <dale@dshendrickson.com>
2016-09-06 15:48:41 -0600

    Random color generator working.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,28 @@
 
-function checkRandomColor() {
-  var color = '#FFFFFF'
-  while color === '#FFFFFF' {
-    color = getRandomColor ()
+function getRandomColor() {
+  var color = '#FFFFFF';
+  while (color === '#FFFFFF') {
+    debugger;
+    color = generateRandomColor();
+    debugger;
   }
   return color;
-}
+};
 
 // http://stackoverflow.com/questions/1484506/random-color-generator-in-javascript
-function getRandomColor() {
+function generateRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++ ) {
         color += letters[Math.floor(Math.random() * 16)];
-    }
+    };
     return color;
-}
+};
+
+//*** Events ***//
+
+$('.btn-random').on('click', function () {
+  debugger;
+  var color = getRandomColor();
+  $('.container').css('background', color);
+});
